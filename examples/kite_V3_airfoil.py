c40f534089e26b1b23de2fd154ea336bb7062e37
r.schmehl@tudelft.nl <r.schmehl@tudelft.nl>
2024-10-09 16:14:12 +0200

    remove unused path

--- examples/kite_V3_airfoil.py
+++ examples/kite_V3_airfoil.py
@@ -9,15 +9,9 @@ Created on Thu Aug 11 11:03:50 2022
 
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 from pathlib import Path
 from itertools import product, combinations
-import vplot3d.vplot3d as vplot3d_file
 from vplot3d.vplot3d import init_view, Polygon, save_svg_tex
-
-# Set this with environment variable PYTHONPATH
-#lib_path = Path(vplot3d_file.__file__).parent
-#sys.path.append(str(lib_path))
 
 # Folder with shared data
 dat_path = Path.cwd().parent / 'data'
--- examples/kite_kinematics_3d.py
+++ examples/kite_kinematics_3d.py
@@ -9,14 +9,8 @@ Created on Thu Aug 11 11:03:50 2022
 
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 from pathlib import Path
-import vplot3d.vplot3d as vplot3d_file
 from vplot3d.vplot3d import init_view, Line, Vector, Point, Arc, ArcMeasure, Polygon, save_svg_tex
-
-# Set this with environment variable PYTHONPATH
-#lib_path = Path(vplot3d_file.__file__).parent
-#sys.path.append(str(lib_path))
 
 # Folder with shared data
 dat_path = Path.cwd().parent / 'data'
